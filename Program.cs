8275406728383169f8ca4a22986116d1212edba1
millakortelainen <milla.kortelainen@gmail.com>
2022-03-03 14:58:52 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Functions
+{
+    public static int ScanString()
+    {
+        Console.WriteLine("hello!");
+        return 77;
+    }
+}
+
+class MainTestClass
+{
+    static int Main(string[] args)
+    {
+        Functions.Test();
+        return -1;
+    }
+}
